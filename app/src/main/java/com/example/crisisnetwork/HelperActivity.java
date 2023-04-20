743e2ac1a6b626a27895745546ebe37755bdda1b
MonikaSomu <116198320+MonikaSomu@users.noreply.github.com>
2023-04-20 19:32:10 -0400

    Video is playing on helper's side.

--- app/src/main/java/com/example/crisisnetwork/HelperActivity.java
+++ app/src/main/java/com/example/crisisnetwork/HelperActivity.java
@@ -3,12 +3,24 @@ package com.example.crisisnetwork;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class HelperActivity extends AppCompatActivity {
+    VideoView vv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_helper);
+        //vv is the id of videoview
+        vv = findViewById(R.id.vv);
+        vv.setVideoPath("android.resource://com.example.crisisnetwork/" + R.raw.chicago2);
+
+        //This is for getting the play, pause, stop buttons.
+        MediaController med = new MediaController(this);
+        vv.setMediaController(med);
+        med.setAnchorView(vv);
+        vv.start();
     }
 }
